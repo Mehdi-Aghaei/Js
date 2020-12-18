5409967a3c8a9a6ad8313bc7910bae98054da01c
Mehdi Aghaei <mahdiaghaei355@gmail.com>
2020-12-18 20:32:35 +0100

    write with const

--- P2/p2.js
+++ P2/p2.js
@@ -1,15 +1,15 @@
 // get input
-let filterInput = document.querySelector("#filterInput");
+const filterInput = document.querySelector("#filterInput");
 //add event listener
 filterInput.addEventListener('keyup',filterNames);
 function filterNames() {
     // get value
-    let filterValue = document.querySelector("#filterInput").value.toUpperCase();
+    const filterValue = document.querySelector("#filterInput").value.toUpperCase();
     const ul = document.querySelector("#names");
     const li = ul.querySelectorAll('li.list-group-item');
     // loop throgh
     for(let i=0;i < li.length;i++){
-        let a = li[i].getElementsByTagName('a')[0];
+        const a = li[i].getElementsByTagName('a')[0];
         // if match
         if(a.innerHTML.toUpperCase().indexOf(filterValue)> -1){
             li[i].style.display='';
